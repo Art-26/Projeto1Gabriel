a3276a6ae934d5045b992e31bd5e4467ede19df5
Art-26 <arthurortbach@gmail.com>
2026-03-27 13:00:49 -0300

    Criação completa da classe ABB, após a criação do método buscarRecursivo, foram criados outros métodos como removerRecursivo, encontrarMinimo e também o metodo para obtermos e calcularmos a altura da árvore binária de buscas.

--- TrabalhosGabriel/src/modelos/ABB.java
+++ TrabalhosGabriel/src/modelos/ABB.java
@@ -26,7 +26,7 @@ public class ABB<T extends Comparable<T>> {
 	}
 		
 		public boolean buscar(T valor) {
-			return buscarResponsivo(raiz, valor);
+			return buscarRecursivo(raiz, valor);
 		}
 		
 		private boolean buscarRecursivo(No no, T valor) {
@@ -34,5 +34,43 @@ public class ABB<T extends Comparable<T>> {
 			if (no == null) return false;
 			if (valor.equals(no.valor)) return true;
 			return valor.compareTo(no.valor) < 0 ? buscarRecursivo(no.esquerdo, valor) : buscarRecursivo(no.direito, valor);
+		}
+		
+		public void remover(T valor) {
+			raiz = removerRecursivo(raiz, valor);
+		}
+		
+		private No removerRecursivo(No no, T valor) {
+			
+			if (no == null) return null;
+			if (valor.compareTo(no.valor) <0) no.esquerdo = removerRecursivo(no.esquerdo, valor);
+			else if (valor.compareTo(no.valor) > 0) no.direito = removerRecursivo(no.direito, valor);
+			else {
+				if(no.esquerdo == null) return no.direito;
+				if(no.direito == null) return no.esquerdo;
+				no.valor = encontrarMinimo(no.direito);
+				no.direito = removerRecursivo(no.direito, no.valor);
+			}
+			
+			return no;
+		}
+		
+		private T encontrarMinimo(No no) {
+			T min = no.valor;
+			while (no.esquerdo != null) {
+				min = no.esquerdo.valor;
+				no = no.esquerdo;
+			}
+			
+			return min;
+		}
+		
+		public int obterAltura() {
+			return calcularAltura(raiz);
+		}
+		
+		private int calcularAltura(No no) {
+			if (no == null) return -1;
+			return 1 + Math.max(calcularAltura(no.esquerdo), calcularAltura(no.direito));
 		}
 }
