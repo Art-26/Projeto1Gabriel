ba52356f394d4b147f42a0f58d1c2ce77501afbe
Art-26 <arthurortbach@gmail.com>
2026-04-08 12:05:26 -0300

    Criação das classes BuscaBinaria.java e BuscaSequencial.java

---
+++ Projeto2Gabriel/src/BuscaBinaria.java
@@ -0,0 +1,23 @@
+
+public class BuscaBinaria {
+	public static int buscar(int[] vetor, int alvo) {
+		int esquerda = 0;
+		int direita = vetor.length - 1;
+		
+		while(esquerda <= direita) {
+			int meio = esquerda + (direita - esquerda) / 2;
+			
+			if(vetor[meio] == alvo) { 
+				return meio;
+			}
+			
+			if(vetor[meio] < alvo) {
+				esquerda = meio + 1;
+			} else {
+				direita = meio - 1;
+			}
+		}
+		
+		return -1;
+	}
+}
